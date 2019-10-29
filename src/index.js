1868e2cd61f621519c181bf14cfffef1e6decca9
Mark Qi <markqi@amazon.com>
2019-10-28 21:57:35 -0700

    completed https://www.robinwieruch.de/node-express-server-rest-api

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import 'dotenv/config';
-import cors from 'cors';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
+import models from "./models";
+import routes from "./routes";
 
 const app = express();
 
-app.use(cors());
+// Accessing the payload of an HTTP POST
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// custom middleware to set a user
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1]
+  };
+  next();
 });
 
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
+
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
+  console.log(`Example app listening on port ${process.env.PORT}!`)
 );
